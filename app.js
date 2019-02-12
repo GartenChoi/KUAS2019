ad000e6028f2aa8bd8ddb253ca3fe01fbe1b3356
Garten Choi <jedo0328@naver.com>
2019-02-13 08:05:57 +0900

    init

--- app.js
+++ app.js
@@ -5,18 +5,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
 
-const mongoose=require('mongoose'),db=mongoose.connection;
-
-mongoose.connect('mongodb://KUAS:KUAS2019' +
-    '@cluster0-shard-00-00-pp6sh.mongodb.net:27017,' +
-    'cluster0-shard-00-01-pp6sh.mongodb.net:27017,' +
-    'cluster0-shard-00-02-pp6sh.mongodb.net:27017',
-    {
-        useNewUrlParser: true,
-        promiseLibrary: global.Promise
-    });
+// const mongoose=require('mongoose'),db=mongoose.connection;
+//
+// mongoose.connect('mongodb://KUAS:kuas2019@cluster0-shard-00-00-pp6sh.mongodb.net:27017,cluster0-shard-00-01-pp6sh.mongodb.net:27017,cluster0-shard-00-02-pp6sh.mongodb.net:27017',
+//     {
+//         useNewUrlParser: true,
+//         promiseLibrary: global.Promise
+//     });
 
 var app = express();
 
@@ -30,23 +27,39 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+    res.locals.kuas={
+        url:'https://kuas2019.herokuapp.com',
+        mainColor:'#f88',
+        warningColor:'#f00',
+        successColor:'#298480',
+        lightColor:'#fff',
+        darkColor:'rgba(0,0,0.7)',
+        footerColor:'rgba(0,0,0,0.2)'
+    };
+    res.locals.mapUrl=function(uName,mName){
+        return mName?('/map?school='+encodeURI(uName)+'&major='+encodeURI(mName)):('/map?school='+encodeURI(uName));
+    };
+    next();
+});
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
