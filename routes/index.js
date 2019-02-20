7c4df2c09ffb40cd7e23881faa280a0b04fabb56
Garten Choi <jedo0328@naver.com>
2019-02-20 19:00:42 +0900

    ok

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/googleff8ee2516444f278.html', function(req, res, next) {
+    res.send('google-site-verification: googleff8ee2516444f278.html');
+});
 
 module.exports = router;
