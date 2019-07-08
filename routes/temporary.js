ee72e8232ab535f78f43fb86e98dee7b428360e4
Garten Choi <jedo0328@naver.com>
2019-07-09 01:28:40 +0900

    d

--- routes/temporary.js
+++ routes/temporary.js
@@ -1,29 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const ftp=require('ftp');
 
-var mongoose = require('mongoose');
-var db = mongoose.connection;
-mongoose.connect('mongodb://root:!majorofwkqtybj0816@52.231.158.65/admin', {useNewUrlParser: true, promiseLibrary: global.Promise});
-var majorSchema = mongoose.Schema({
-  name: {type: String, require: true},
-  univname: {type: String},
-  wordclouds:{type:Array}
-});
-const Major = mongoose.model('major', majorSchema);
-router.get('/wordclouds', async function(req, res, next) {
-  let ftpClient=new ftp();
-  ftpClient.connect({
-    host: 'jedo0328.cafe24.com',
-    user: 'jedo0328',
-    password:'jeidoubleu0328'
-  });
-  ftpClient.on('ready',function(){
-    ftpClient.list('/MajorMap/chapter2.2/wordcloud',async function(err,list){
-        const majorlength=await Major.countDocuments({'wordclouds.0': {'$exists': true}});
-        const majors = await Major.find({'wordclouds.0': {'$exists': true}},{'name':1,'univname':1});
-        return res.render('temporary/wordclouds', {ftplength:list.length,majors:majors,majorlength:majorlength});
-    })
-  });
+router.get('/game', async function(req, res, next) {
+  return res.render('temporary/game', {});
 });
 module.exports = router;
